81d7d583e4713417c06137d85c169c04aa9b6970
Joshua Raslowksy <43376272+joshraslowsky@users.noreply.github.com>
2023-03-28 18:57:34 -0400

    Updates to Use HTTPClient

--- SDETScreeningProject/src/testing/api/Rest.java
+++ SDETScreeningProject/src/testing/api/Rest.java
@@ -1,13 +1,10 @@
 package testing.api;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.HashMap;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.ContentType;
@@ -28,32 +25,23 @@ public class Rest {
      * Output response int code
      * @throws Exception 
      */
-    public static int sendGetRequest(String url)throws Exception{
-    	URL obj = new URL(url);
-    	//Setup URL Connection with Given URL
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-		//Set Request to Get
-		con.setRequestMethod("GET");
-		con.setRequestProperty("User-Agent", "Test");
-		int responseCode = con.getResponseCode();
-		//Output Response Code
-		System.out.println("GET Response Code :: " + responseCode);
-		//If Response is 200 or HTTP Success then read response
-		if (responseCode == 200) { 
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-			// print result
-			System.out.println(response.toString());
-		} else {
-			System.out.println("GET Request was unsuccesful.");
-		}
-		return responseCode;
-    }
+    /**
+     * Sends a POST request
+     *
+     * @param url, payload
+     *            
+     * Output response int code
+     * @throws Exception 
+     */
+    public static int sendGetRequest(String url) throws Exception {
+
+		 HttpClient httpClient = HttpClientBuilder.create().build();
+	     HttpGet request = new HttpGet(url);
+
+	     HttpResponse response = httpClient.execute(request);
+	     return response.getStatusLine().getStatusCode();
+
+	}
     
   
     /**
@@ -65,32 +53,15 @@ public class Rest {
      * @throws Exception 
      */
     public static int sendDeleteRequest(String url)throws Exception{
-    	URL obj = new URL(url);
-    	//Setup URL Connection with Given URL
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-		//Set Request to Get
-		con.setRequestMethod("DELETE");
-		con.setRequestProperty("User-Agent", "Test");
-		int responseCode = con.getResponseCode();
-		//Output Response Code
-		System.out.println("GET Response Code :: " + responseCode);
-		//If Response is 200 or HTTP Success then read response
-		if (responseCode == 200) { 
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-			// print result
-			System.out.println(response.toString());
-		} else {
-			System.out.println("Delete Request was unsuccesful");
-		}
-		return responseCode;
+		 HttpClient httpClient = HttpClientBuilder.create().build();
+	     HttpDelete request = new HttpDelete(url);
+
+	     HttpResponse response = httpClient.execute(request);
+	     return response.getStatusLine().getStatusCode();
     }
     
+
+ 
     
     /**
      * Sends a POST request
